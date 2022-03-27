31251399e42f9358f1601626beca25c3c43ad438
TheSlavik <thescmtop@gmail.com>
2022-03-28 02:24:18 +0300

    Did some fixes

--- consoleApp/src/main/java/org/example/consoleApp/StoreApp.java
+++ consoleApp/src/main/java/org/example/consoleApp/StoreApp.java
@@ -1,13 +1,9 @@
 package org.example.consoleApp;
 
-import org.example.domain.Category;
-import org.example.domain.Product;
-import org.example.store.RandomStorePopulator;
 import org.example.store.Store;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.lang.reflect.Constructor;
 
 public class StoreApp {
 
@@ -15,8 +11,8 @@ public class StoreApp {
 
     public static void main(String[] args) {
         StoreApp app = new StoreApp();
-        app.generateProducts();
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        app.store.generateProducts();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         loop:
         while (true) {
             try {
@@ -26,7 +22,7 @@ public class StoreApp {
                         "top" - print the five most expensive products;
                         "quit" - exit app.
                         """);
-                switch (r.readLine()) {
+                switch (reader.readLine()) {
                     case "sort":
                         app.store.sort();
                         break;
@@ -40,28 +36,6 @@ public class StoreApp {
                 }
             } catch (Exception e) {
                 e.printStackTrace();
-            }
-        }
-    }
-
-    public void generateProducts() {
-        try {
-            RandomStorePopulator r = new RandomStorePopulator();
-            Constructor<Product> con = Product.class.getConstructor(String.class, Integer.TYPE, Double.TYPE);
-            for (Category ctg : store.getCategories()) {
-                for (int i = 0; i < 5; i++) {
-                    ctg.getProducts().add(con.newInstance(r.getName(ctg.getName()), r.getRate(), r.getPrice()));
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void printProducts() {
-        for (Category category : store.getCategories()) {
-            for (Product product : category.getProducts()) {
-                System.out.println(product);
             }
         }
     }
